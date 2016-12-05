9903f4110c4b12ddcc97bfffe527a6bdb0416d28
Saul <digitalh2o@gmail.com>
2016-12-04 22:40:08 -0600

    Routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
   def index
   end
 
+  def new
+  end
+
   def edit
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'about', to: 'home#edit'
+  get 'coaches', to: 'home#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
